6e1d86945e95032f933c573d51033763cffdd5c9
alexKuzovkov <alexkuzovkov0307@gmail.con>
2026-08-15 13:57:47 +0300

    Remove legacy outbox implementation and obsolete files

--- init-db.sh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE users_db;
-    CREATE DATABASE events_db;
-    CREATE DATABASE commissions_db;
-    CREATE DATABASE wallets_db;
-EOSQL
